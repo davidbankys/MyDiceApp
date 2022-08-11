da10bc064dd09e1acd0d0fb6c2e087150d8fe15b
David Bankole Abel <ballack_davidabel@yahoo.com>
2022-08-11 06:44:03 -0400

    declaration&binding

--- app/src/main/java/com/db/mydiceapp/Dice.java
+++ app/src/main/java/com/db/mydiceapp/Dice.java
@@ -1,2 +1,28 @@
 package com.db.mydiceapp;
 
+public class Dice {
+    private String name;
+    private int sides;
+    private int sideUp;
+    public Dice(int sides) {
+        this.sides = sides;
+        this.name = getDiceNameFor(sides);
+        roll();
+    }
+    public void roll() {
+        int roundRandom = (int) Math.round(Math.random() * 10);
+        if (roundRandom == 0) {
+            this.sideUp = 1;
+        } else {
+            this.sideUp = (int) Math.round((roundRandom * this.sides) / 10);
+        }
+    }
+
+    public int getSideUp() {
+        return this.sideUp;
+    }
+    private String getDiceNameFor(int sides) {
+        return this.name = "d" + sides;
+    }
+}
+
--- app/src/main/java/com/db/mydiceapp/MainActivity.java
+++ app/src/main/java/com/db/mydiceapp/MainActivity.java
@@ -33,4 +33,57 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+
 public class MainActivity extends AppCompatActivity {
+    Spinner dPicker;
+    Integer[] arrChooseDiceSide = {4,6,8,10,12,20};
+    EditText edCustomDiceSide;
+    Button addBtn,btnROnce,btnRTwice;
+    TextView first_roll,second_roll;
+    ArrayList<String> arrListViewArray;
+    ListView lvSavedList;
+    LinearLayout lvMain;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bindID();
+    }
+    private void bindID() {
+        dPicker = findViewById(R.id.dPicker);
+        edCustomDiceSide=findViewById(R.id.edCustomDiceSide);
+        addBtn = findViewById(R.id.addBtn);
+        btnROnce = findViewById(R.id.btnROnce);
+        btnRTwice = findViewById(R.id.btnRTwice);
+        first_roll = findViewById(R.id.first_roll);
+        second_roll = findViewById(R.id.second_roll);
+        lvSavedList= findViewById(R.id.lvSavedList);
+        lvMain= findViewById(R.id.lvMain);
+        //initate Shared Pref
+        SharedPrefManager.init(MainActivity.this);
+        setSpinnerData(arrChooseDiceSide);
+        arrListViewArray = new ArrayList<>();
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //adding new array element to spinner
+                String strNewCustomSide = edCustomDiceSide.getText().toString().trim();
+                if(!strNewCustomSide.equalsIgnoreCase(""))
+                {
+                    Integer arrNewSide[]  = new Integer[arrChooseDiceSide.length +1 ];
+                    for (int i = 0; i < arrChooseDiceSide.length; i++) {
+                        //Copy element from old array to new array
+                        arrNewSide[i] = arrChooseDiceSide[i];
+                    }
+                    arrNewSide[arrNewSide.length -1] = Integer.parseInt(strNewCustomSide);
+                    //sorting array
+                    Arrays.sort(arrNewSide);
+                    //copy new array to old array
+                    arrChooseDiceSide= Arrays.copyOf(arrNewSide, arrNewSide.length);
+                    setSpinnerData(arrChooseDiceSide);
+                    Toast.makeText(MainActivity.this ,"Your custom Dice " + strNewCustomSide +" is added to dropdown" ,Toast.LENGTH_SHORT).show();
+                    edCustomDiceSide.setText("");
+                }
+            }
+        });
+
